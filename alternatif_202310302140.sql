f6cfe9742732ecab301222be374391f50dd400a2
Syarif17 <138609642+Syarif47@users.noreply.github.com>
2023-10-30 21:52:05 -0700

    Add files via upload

---
+++ alternatif_202310302140.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.alternatif ("Nama Handphone",ram,memori,chipset,layar,harga,baterai,alternatif,nilaiakhir,"Rank") VALUES
+	 ('Samsung Galaxy S21 Ultra','12GB','128GB','Qualcomm Snapdragon 888','Dynamic AMOLED 2X, 6.8 inches ','14.000.000','5000mAh',2.7613754,0.11659072,4),
+	 ('Iphone 13 Pro ','6GB','128GB','Apple A15 Bionic','Super Retina XDR OLED, 6.1 inches','17.900.000','3095mAh',1.6397563,0.06923375,10),
+	 ('One Plus 9 Pro','8GB','128GB','Qualcomm Snapdragon 888','Fluid AMOLED, 6.7 inches','6.800.000','4500mAh',1.8156235,0.07665922,9),
+	 ('Xiaomi Mi 11','8GB','128GB','Qualcomm Snapdragon 888','AMOLED, 6.8 inches','9.400.000','4600mah',2.2877402,0.09659291,7),
+	 ('Google Pixel 6 pro','12GB','128GB','Google Tensor ','P-OLED, 6.7 inches','14.000.000','5000mAh',2.9981098,0.12658612,1),
+	 ('Asus Rog Phone 5','8GB','256GB','Qualcomm Snapdragon 888','AMOLED, 6.7 inches','9.000.000','6000mAh',2.4207346,0.1022082,5),
+	 ('Sony Xperia 1 ','8GB','256GB','Qualcomm Snapdragon 888','4K OLED, 6.5 inches','16.900.000','4500mAh',2.3064554,0.097383104,6),
+	 ('Samsung Galaxy Z Fold 3','12GB','256GB','Qualcomm Snapdragon 888','Dynamic AMOLED 2X, 7.6 inches','9.700.000','4400mAh',2.7550411,0.11632328,3),
+	 ('Oppo Find X3 Pro','12GB','256GB','Qualcomm Snapdragon 888','AMOLED, 6.7 inches','14.000.000','4500mAh',2.787892,0.117710315,2),
+	 ('Huawei P50 Pro','8GB','128GB','Qualcomm Snapdragon 888','OLED, 6.6 inches','11.000.000','4360mAh',1.9116204,0.08071239,8);
